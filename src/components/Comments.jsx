e1efc2e8aac722e95558513695dc69b9359ed724
P-McD <pmcdonnell220@gmail.com>
2023-04-06 13:33:48 +0100

    small bug fixes

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState, useContext } from "react";
 import { commentsHandler, postCommentHandler } from "../api";
 import CommentCard from "./CommentCard";
 import { UserContext } from "../contexts/UserContext";
-import "../css/Comments.css"
+import "../css/Comments.css";
+import Loading from "./Loading";
 
 function Comments() {
   const { user } = useContext(UserContext);
@@ -13,13 +14,13 @@ function Comments() {
   const [notSignedIn, setNotSignedIn] = useState(false);
   const [commentError, setCommentError] = useState(false);
   const [inputComment, setInputComment] = useState("");
-  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     commentsHandler(article_id).then((data) => {
       setComments(data);
-      setLoading(false)
+      setLoading(false);
     });
   }, [article_id]);
   const typeComment = (event) => {
@@ -34,11 +35,11 @@ function Comments() {
     } else if (inputComment.length < 2) {
       setCommentError(true);
     } else {
-      setButtonDisabled(true)
+      setButtonDisabled(true);
       postCommentHandler({ article_id, user, inputComment }).then(
         (returnedComment) => {
-          setButtonDisabled(false)
-          setInputComment("")
+          setButtonDisabled(false);
+          setInputComment("");
           setComments((currentComments) => {
             return [returnedComment, ...currentComments];
           });
@@ -46,57 +47,66 @@ function Comments() {
       );
     }
   };
-
-  return (
-    <div className="comments">
-      <h3>Comments</h3>
-      <form id="post-comment">
-        <label htmlFor="comment-box">Join the Conversation!</label>
-        <br />
-        <textarea id="comment-box" onChange={typeComment} value={inputComment}></textarea>
-        <br />
-        <button
-          id="submit-comment"
-          onClick={(event) => {
-            event.preventDefault();
-            submitComment();
-          }} disabled={buttonDisabled}
-        >
-          {buttonDisabled ? "Please Wait..." : "Post a Comment"}
-        </button>
-        <span id="comment-signin" className="error-box">
-          {notSignedIn ? (
-            <p>
-              You must be signed in to post a comment.
-              <Link to="/signin">Sign In</Link>
-            </p>
-          ) : null}
-        </span>
-        <span id="comment-length-error" className="error-box">
-          {commentError ? (
-            <p>Comments must contain at least 2 characters. Please try again</p>
-          ) : null}
-        </span>
-      </form>
-      {/* <div></div> */}
-      {comments.length === 0 ? (
-        <div className="no-comments">
-          <h4>Comments</h4>
-          <p>Be the first to comment!</p>
-        </div>
-      ) : (
-        <ul>
-          {comments.map((comment) => {
-            return (
-              <li key={comment.comment_id} >
-                <CommentCard comment={comment} />
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </div>
-  );
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="comments">
+        <h3>Comments</h3>
+        <form id="post-comment">
+          <label htmlFor="comment-box">Join the Conversation!</label>
+          <br />
+          <textarea
+            id="comment-box"
+            onChange={typeComment}
+            value={inputComment}
+          ></textarea>
+          <br />
+          <button
+            id="submit-comment"
+            onClick={(event) => {
+              event.preventDefault();
+              submitComment();
+            }}
+            disabled={buttonDisabled}
+          >
+            {buttonDisabled ? "Please Wait..." : "Post a Comment"}
+          </button>
+          <span id="comment-signin" className="error-box">
+            {notSignedIn ? (
+              <p>
+                You must be signed in to post a comment.
+                <Link to="/signin">Sign In</Link>
+              </p>
+            ) : null}
+          </span>
+          <span id="comment-length-error" className="error-box">
+            {commentError ? (
+              <p>
+                Comments must contain at least 2 characters. Please try again
+              </p>
+            ) : null}
+          </span>
+        </form>
+        {comments.length === 0 ? (
+          <div className="no-comments">
+            <h4>Comments</h4>
+            <p>Be the first to comment!</p>
+          </div>
+        ) : (
+          <ul>
+            {comments.map((comment) => {
+              return (
+                <li key={comment.comment_id}>
+                  <CommentCard comment={comment} />
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Comments;
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -10,7 +10,7 @@ export default function SignIn() {
   const [usernameErr, setUsernameErr] = useState(false);
   const [passwordErr, setPasswordErr] = useState(false);
   const [inputPassword, setInputPassword] = useState("");
-  const [loadSignin, setLoadSignin] = useState(false)
+  const [loadSignin, setLoadSignin] = useState(false);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -28,20 +28,21 @@ export default function SignIn() {
     setInputPassword(event.target.value);
   };
   const signInButton = (event) => {
-    setLoadSignin(true)
+    setLoadSignin(true);
     event.preventDefault();
     if (inputPassword === "") {
-      setLoadSignin(false)
-      return setPasswordErr(true) 
+      setLoadSignin(false);
+      return setPasswordErr(true);
     }
     signInHandler(inputUsername).then((users) => {
       users.map((user) => {
         if (user.username === inputUsername) {
-          setUser(user);
-          setLoadSignin(false)
+          setLoadSignin(false);
+          return setUser(user);
         } else {
-          setLoadSignin(false)
-        return setUsernameErr(true);}
+          setLoadSignin(false);
+          return setUsernameErr(true);
+        }
       });
     });
   };
@@ -50,7 +51,7 @@ export default function SignIn() {
       <h3>Welcome!</h3>
       <p>Login to your account</p>
       <div className="login-box">
-        <form className="signin-form"id="signin">
+        <form className="signin-form" id="signin">
           <label htmlFor="username">Username: </label>
           <input
             id="username"
@@ -81,11 +82,15 @@ export default function SignIn() {
         <span className="error-box">
           {usernameErr ? (
             <p>
-              Username not found, please check your login details and try again (Hint: try "jessjelly")
+              Username not found, please check your login details and try again
+              (Hint: try "jessjelly")
             </p>
           ) : null}
         </span>
-        <span className="error-box"> {loadSignin ? <p>Signing in, please wait...</p> : null}</span>
+        <span className="error-box">
+          {" "}
+          {loadSignin ? <p>Signing in, please wait...</p> : null}
+        </span>
       </div>
     </div>
   );
